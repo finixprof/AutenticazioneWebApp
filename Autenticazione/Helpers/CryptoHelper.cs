58343780254849f0d092d1f0a5e780fd5a949ec4
finix77 <32540149+finix77@users.noreply.github.com>
2022-03-01 09:09:13 +0100

    aggiornato cryptohelper

--- Autenticazione/Helpers/CryptoHelper.cs
+++ Autenticazione/Helpers/CryptoHelper.cs
@@ -10,99 +10,76 @@ namespace Autenticazione.Helpers
 {
     public static class CryptoHelper
     {
-        // This constant is used to determine the keysize of the encryption algorithm in bits.
-        // We divide this by 8 within the code below to get the equivalent number of bytes.
-        private const int Keysize = 256;
+        private static string _cryptoData = "classe5D2021-22";
 
-        // This constant determines the number of iterations for the password bytes generation function.
-        private const int DerivationIterations = 1000;
-
-        private const string PassPhrase = "classe5D2021-22";
-
-        public static string Encrypt(string plainText)
+        private static byte[] Encrypt(byte[] dData, byte[] outerKey, byte[] IV)
         {
-            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
-            // so that the same Salt and IV values can be used when decrypting.  
-            var saltStringBytes = Generate256BitsOfRandomEntropy();
-            var ivStringBytes = Generate256BitsOfRandomEntropy();
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            using (var password = new Rfc2898DeriveBytes(PassPhrase, saltStringBytes, DerivationIterations))
+            using (MemoryStream memoryStream = new MemoryStream())
             {
-                var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
+                Rijndael key = Rijndael.Create();
+                key.Key = outerKey;
+                key.IV = IV;
+
+                using (CryptoStream cryptoStream =
+                    new CryptoStream(memoryStream, key.CreateEncryptor(),
+                        CryptoStreamMode.Write))
                 {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-                    using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
-                    {
-                        using (var memoryStream = new MemoryStream())
-                        {
-                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                            {
-                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                                cryptoStream.FlushFinalBlock();
-                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-                                var cipherTextBytes = saltStringBytes;
-                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Convert.ToBase64String(cipherTextBytes);
-                            }
-                        }
-                    }
+                    cryptoStream.Write(dData, 0, (int)dData.Length);
+                    cryptoStream.Close();
                 }
+                byte[] array = memoryStream.ToArray();
+                return array;
             }
         }
 
-        public static string Decrypt(string cipherText)
+        public static string Encrypt(string dText)
         {
-            // Get the complete stream of bytes that represent:
-            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
-            var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
-            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
-            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
-            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
-            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
 
-            using (var password = new Rfc2898DeriveBytes(PassPhrase, saltStringBytes, DerivationIterations))
+            byte[] bytes = Encoding.Unicode.GetBytes(dText);
+            PasswordDeriveBytes passwordDeriveByte =
+                new PasswordDeriveBytes(CryptoHelper._cryptoData,
+                    new byte[] { 0x44, 0x6f, 0x74, 0x4e, 0x65, 0x74, 0x57,
+                0x6f, 0x72, 0x6b, 0x65, 0x72, 0x73, 0x2e, 0x47,
+                0x6f, 0x74, 0x74, 0x61 });
+            byte[] encryptedArray = CryptoHelper.Encrypt(bytes,
+                passwordDeriveByte.GetBytes(32), passwordDeriveByte.GetBytes(16));
+            return Convert.ToBase64String(encryptedArray);
+
+        }
+
+        private static byte[] Decrypt(byte[] eData, byte[] outerKey, byte[] IV)
+        {
+
+            using (MemoryStream memoryStream = new MemoryStream())
             {
-                var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
+                Rijndael key = Rijndael.Create();
+                key.Key = outerKey;
+                key.IV = IV;
+                using (CryptoStream cryptoStream =
+                    new CryptoStream(memoryStream, key.CreateDecryptor(),
+                        CryptoStreamMode.Write))
                 {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-                    using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
-                    {
-                        using (var memoryStream = new MemoryStream(cipherTextBytes))
-                        {
-                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                            {
-                                var plainTextBytes = new byte[cipherTextBytes.Length];
-                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
-                            }
-                        }
-                    }
+                    cryptoStream.Write(eData, 0, (int)eData.Length);
+                    cryptoStream.Close();
                 }
+                byte[] array = memoryStream.ToArray();
+                return array;
             }
         }
 
-        private static byte[] Generate256BitsOfRandomEntropy()
+        public static string Decrypt(string eText)
         {
-            var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
-            using (var rngCsp = new RNGCryptoServiceProvider())
-            {
-                // Fill the array with cryptographically secure random bytes.
-                rngCsp.GetBytes(randomBytes);
-            }
-            return randomBytes;
+
+            byte[] dataArray = Convert.FromBase64String(eText);
+            PasswordDeriveBytes passwordDeriveByte =
+                new PasswordDeriveBytes(CryptoHelper._cryptoData,
+                    new byte[] { 0x44, 0x6f, 0x74, 0x4e, 0x65, 0x74, 0x57,
+                0x6f, 0x72, 0x6b, 0x65, 0x72, 0x73, 0x2e, 0x47, 0x6f,
+                0x74, 0x74, 0x61 });
+            byte[] hiddenArray = CryptoHelper.Decrypt(dataArray,
+                passwordDeriveByte.GetBytes(32), passwordDeriveByte.GetBytes(16));
+            return Encoding.Unicode.GetString(hiddenArray);
+
         }
 
         //cifratura one way
